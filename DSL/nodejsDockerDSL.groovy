e77731094abb0db39e7879c665c573c416e38d8b
Juan David Rivera <60453003+Juan9507@users.noreply.github.com>
2023-01-12 08:19:31 -0500

    Update nodejsDockerDSL.groovy

--- DSL/nodejsDockerDSL.groovy
+++ DSL/nodejsDockerDSL.groovy
@@ -1,9 +1,9 @@
 job('Aplicacion Node.js Docker DSL') {
     description('Aplicación Node JS Docker DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/nodejsapp.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
-            node / gitConfigEmail('macloujulian@gmail.com')
+        git('https://github.com/Juan9507/nodejsapp.git', 'master') { node ->
+            node / gitConfigName('juan9507')
+            node / gitConfigEmail('juandavidnaranjo75@gmail.com')
         }
     }
     triggers {
